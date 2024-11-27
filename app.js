368cd3d4663e01f8d637fbebc8d64b3734325eb0
mgtteeg <thalesferreiradasilva07@gmail.com>
2024-11-27 19:17:43 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// Exemplo de interação: login (simples, para fins de demonstração)
+document.getElementById("login-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Você entrou no sistema!");
+});
